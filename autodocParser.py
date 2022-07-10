4f45847e4ac7bdb209013ab6c06a29acfab8713d
g.sukhomlin@skillline.ru <gera.sukhomlin@gmail.com>
2022-07-10 23:12:02 +0200

    Достаёт инфу только из категорий, надо переделать что доставал из узлов

--- autodocParser.py
+++ autodocParser.py
@@ -1,94 +1,111 @@
 import asyncio
+import json
 from pprint import pprint
+import random
 import aiohttp
-import requests
+import pandas
+from tqdm import tqdm
+# pip install xlsxwriter
+# pip install openpyxl
+# pip install pandas
+# pip install aiohttp
+# pip install asyncio
 
 
 class AutodocParser:
     def __init__(self, data):
-        # Здесь в ответе есть Ssd
-        self.keyUrl = "https://catalogoriginal.autodoc.ru/api/catalogs/original/cars/Z8NAJL01051621776/modifications?clientId=375"
-        self.SsdKey = "KwFZbXxtWDkmFV5eKlhqWwEVNTIsXFtdW0xKSQ8qFk4CHk9VEGppJUhBTgIZT1UQZhglKycgW1tdWgMKElpdWlpJRk8KUx4aMDcQAAAAAKU0G6w"
-        # В каждом запросе используется этот SsdKey
-        # self.carInfoUrl = "Request URL: https://catalogoriginal.autodoc.ru/api/catalogs/original/catalogCodes/NISSAN201809?ssd=$*KwFZbXxtWDkmFV5eKlhqWwEVNTIsXFtdW0xKSQ8qFk4CHk9VEGppJUhBTgIZT1UQZhglKycgW1tdWgMKElpdWlpJRk8KUx4aMDcQAAAAAKU0G6w=$"
-        self.categoriesUrl = "https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/NISSAN201809/cars/0/categories?ssd=$*KwFZbXxtWDkmFV5eKlhqWwEVNTIsXFtdW0xKSQ8qFk4CHk9VEGppJUhBTgIZT1UQZhglKycgW1tdWgMKElpdWlpJRk8KUx4aMDcQAAAAAKU0G6w=$"
-        self.quickGroupsUrl = "Request URL: https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/NISSAN201809/cars/0/quickgroups?ssd=$*KwFZbXxtWDkmFV5eKlhqWwEVNTIsXFtdW0xKSQ8qFk4CHk9VEGppJUhBTgIZT1UQZhglKycgW1tdWgMKElpdWlpJRk8KUx4aMDcQAAAAAKU0G6w=$"
-        self.tokenUrl = "https://auth.autodoc.ru/token"
         self.timeout = 5
-        self.sessionTimeout = aiohttp.ClientTimeout(total=None, sock_connect=self.timeout, sock_read=self.timeout)
+        self.sessionTimeout = aiohttp.ClientTimeout(
+            total=None, sock_connect=self.timeout, sock_read=self.timeout)
         self.connector = aiohttp.TCPConnector(ssl=False, limit=10000)
-        self.session = aiohttp.ClientSession(connector=self.connector, timeout=self.sessionTimeout)
+        self.session = aiohttp.ClientSession(
+            connector=self.connector, timeout=self.sessionTimeout)
         self.data = data
         self.tokenPostData = {
             "username": "KNG-16078",
             "password": "6sqqSZ77PHZPmEL",
             "grant_type": "password"
         }
+        self.inputFilename = "input.xlsx"
+        self.ouputFilename = "output.xlsx"
 
 
     def run(self):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.startParsing())
-        
 
-    async def startParsing(self): 
-        requests = []       
-        ### * Запросы по VIN
-        for item in self.data:
-            request = self.makePrimaryRequest(item["VIN"], item["CLIENT_ID"])
-            requests.append(request)       
-        responses = await asyncio.gather(*requests)
-        requests.clear()
 
-        ### *  Основная информация по машине
+    async def startParsing(self):
+        # Строки с результатами
+        outputData = []
+
+        requestsList = []
+        # * Запросы по VIN
+        for vin in self.data:
+            request = self.makePrimaryRequest(vin)
+            requestsList.append(request)
+        responses = await asyncio.gather(*requestsList)
+        requestsList.clear()
+
+        # *  Основная информация по машинам
         primaryData = []
         for response in responses:
             primaryData.append(self.collectPrimaryData(response))
         carsPrimaryInfo = await asyncio.gather(*primaryData)
 
-        ### * Категории запчастей (1-й уровень)
-        requests = []
-        for car in carsPrimaryInfo:
-            categoriesUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{car['Catalog']}/cars/0/categories?ssd={car['Ssd']}"
-            response = await self.session.get(categoriesUrl)
-            carSparePartsCategories = await response.json()
-            ### * Информация о категории запчастей #* (2-й уровень)
-            for sparePart in carSparePartsCategories["items"]:
-                sparePartInfoUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{car['Catalog']}/cars/0/categories/{sparePart['categoryId']}/units?ssd={sparePart['ssd']}"
-                sparePartInfoResponse = await self.session.get(sparePartInfoUrl)
-                sparePartInfoResponseJson = await sparePartInfoResponse.json()
-                for sparePartsInfo in sparePartInfoResponseJson.get("items", []):
-                    sparePartDetailInfoUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{car['Catalog']}/cars/0/units/{sparePartsInfo['unitId']}/spareparts?ssd={sparePartsInfo['ssd']}"
-                    data = {
-                        "content-type": "application/json"
-                    }
-                    # TODO: РАЗОБРАТЬСЯ !!! Тут надо посылать json
-                    sparePartDetailInfoResponse = await self.session.post(sparePartDetailInfoUrl, data=data)
-                    sparePartDetailInfoResponseJson = await sparePartDetailInfoResponse.json()
-                    pprint(sparePartDetailInfoResponseJson)
-                
-                print("\n\n\n")
-        
 
-    async def GetToken(self):
-        headers = {
-            "content-type:" : "application/json;charset=UTF-8"
-        }
-        session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), headers=headers)
-        response = await session.post(self.tokenUrl, self.tokenPostData)
-        print(await response.json())
+        # * Парсинг по VIN по машине
+        requestsList = []
+        for carData in carsPrimaryInfo:
+            requestsList.append(self.parseVINs(carData))
+        carsInfo = await asyncio.gather(*requestsList)
+        self.session.close()
+        self.writeToExcel(carsInfo)
 
 
-        ### * Информация об узлах запчастей #* (2-й уровень)
-        quickGroupsUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/NISSAN201809/cars/0/quickgroups?ssd=$*KwFZbXxtWDkmFV5eKlhqWwEVNTIsXFtdW0xKSQ8qFk4CHk9VEGppJUhBTgIZT1UQZhglKycgW1tdWgMKElpdWlpJRk8KUx4aMDcQAAAAAKU0G6w=$"
+    async def parseVINs(self, carInfo):
+        outputData = []
 
-        await self.session.close()
+        unknownNumber = 0 #! Для некоторых машин нужно это число
+        unknownNumber = 12909 #! Для каких-то это
+
+        categoriesUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{carInfo['Catalog']}/cars/{unknownNumber}/categories?ssd={carInfo['Ssd']}"
+        pprint(carInfo)
+        with open("carInfo.json", "a", encoding="utf-8") as file:
+            file.write(json.dumps(carInfo))
+        print(categoriesUrl)
+        response = await self.session.get(categoriesUrl)
+        carSparePartsCategories = await response.json()
+
+        categories = carSparePartsCategories.get("items", []) if "items" in carSparePartsCategories.keys() else carSparePartsCategories
+
+        for partCategory in tqdm(categories):
+            sparePartInfoUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{carInfo['Catalog']}/cars/0/categories/{partCategory['categoryId']}/units?ssd={partCategory['ssd']}"
+            sparePartInfoResponse = await self.session.get(sparePartInfoUrl)
+            sparePartInfoResponseJson = await sparePartInfoResponse.json()
+            for sparePartsInfo in sparePartInfoResponseJson.get("items", []):
+                sparePartDetailInfoUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{carInfo['Catalog']}/cars/0/units/{sparePartsInfo['unitId']}/spareparts?ssd={sparePartsInfo['ssd']}"
+                data = {
+                    "Ssd": sparePartsInfo['ssd']
+                }
+                sparePartDetailInfoResponse = await self.session.post(sparePartDetailInfoUrl, data=data)
+                sparePartDetailInfoResponseJson = await sparePartDetailInfoResponse.json()
+                spareParts = sparePartDetailInfoResponseJson.get("items", [])
+                for sparePart in spareParts:
+                    outputData.append({
+                        "car": carInfo['Catalog'],
+                        "category": partCategory["name"],
+                        "partName": sparePart["name"],
+                        "partNumber": sparePart["partNumber"]
+                    })
+        return outputData
 
 
+    # Обычные запросы по VIN
     async def collectPrimaryData(self, response):
         carPrimaryInfo = {}
         responseData = await response.json()
-        
+
         try:
             primaryData = responseData["commonAttributes"]
         except Exception as error:
@@ -98,34 +115,35 @@ class AutodocParser:
         for attribute in primaryData:
             carPrimaryInfo[attribute["key"]] = attribute["value"]
         return carPrimaryInfo
-    
 
 
+    async def makePrimaryRequest(self, vin):
+        clientId = random.randint(0, 500)
+        # clientId = 333
 
-
-    async def makePrimaryRequest(self, vin, clientId):
-        # VIN: Z8NAJL01051621776
-        # clientId: 375
         keyUrl = f"https://catalogoriginal.autodoc.ru/api/catalogs/original/cars/{vin}/modifications?clientId={clientId}"
         response = await self.session.get(keyUrl)
         return response
+
     
-
-
-
+    def writeToExcel(self, carInfo):
+        writer = pandas.ExcelWriter(self.ouputFilename, engine='openpyxl')
+        # print(carInfo)
+        for row in carInfo:
+            df = pandas.DataFrame(row)
+            try:
+                sheetName = row[0]["car"]
+            except:
+                sheetName = "empty"
+            # print(sheetName)
+            df.to_excel(writer, sheet_name=sheetName, encoding="utf-8", index=False)
+            writer.save()
+        writer.close()
 
 
 if __name__ == "__main__":
-    data = [
-            {
-                "VIN": "Z8NAJL01051621776",
-                "CLIENT_ID": 375
-            },
-            {
-                "VIN": "WDDMH4EB1FJ311912",
-                "CLIENT_ID": 378
-            },
+    vins = pandas.read_excel("input.xlsx", header=None)
+    data = [vin[0] for vin in vins.values.tolist()]
 
-        ]
     autodocParser = AutodocParser(data)
     autodocParser.run()
